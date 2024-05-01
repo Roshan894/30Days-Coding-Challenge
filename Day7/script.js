a87c821b3a9164a3425927424d1b25e2d734bb76
ROSHAN BHARATHI R <136945078+Roshan894@users.noreply.github.com>
2024-05-01 13:39:55 +0530

    Add files via upload
    Day 7 of web development task - FAQ page

---
+++ Day7/script.js
@@ -0,0 +1,11 @@
+const items = document.querySelectorAll('.accordion-item button');
+function toggleAccording(){
+    const itemToggle = this.getAttribute('aria-expanded');
+    for(i=0;i<items.length;i++){
+        items[i].setAttribute('aria-expanded', 'false');
+    }
+    if(itemToggle == 'false'){
+        this.setAttribute('aria-expanded','true');
+    }
+}
+items.forEach((item) => item.addEventListener('click',toggleAccording));
